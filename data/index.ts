fb9bd7e515e69a6977033bfca0164e214ddc579e
vedu <patilvedant9112@gmail.com>
2024-07-19 15:00:25 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    // { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Services", link: "#Services" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a E-commerce Website",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+
+    {
+      id: 1,
+      title: "Real Estate Modern Website",
+      des: "Modern, animated real estate website using Next.js and GSAP for dynamic property showcases.",
+      img: "/project1.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "/ui.yoom.com",
+    },
+    {
+      id: 2,
+      title: "Gym Modern Website ",
+      des: "Stylish, interactive gym website built with Next.js and GSAP, featuring engaging animations.",
+      img: "/project2.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "/ui.aiimg.com",
+    },
+    {
+      id: 3,
+      title: "Cloth E-commerce Website ",
+      des: "Mobile Friendly ecommerce website with addto cart and payment getway.",
+      img: "/eco.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "/ui.earth.com",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "/ui.apple.com",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+"Vedant's team built an incredible website for our gym. Their attention to detail, user-friendly design, and seamless functionality have truly elevated our online presence. Members can now easily book classes and view schedules. The project was delivered on time and exceeded all our expectations. We couldn't be happier with the results.",
+      name: "Arjun Sharma",
+      title: "FitLife Gym , Pune",
+    },
+    {
+      quote:
+"Working with Vedant was a fantastic experience. They designed a stunning and intuitive website for our restaurant that perfectly captures our brand’s essence. The online reservation system they implemented has streamlined our operations, and our customers love the new look. Their professionalism and creativity are truly impressive.",
+      name: "Rajiv Kumar",
+      title: " SpiceRoute Restaurant , Pune",
+    },
+    {
+      quote:
+"Vedant's agency delivered an exceptional e-commerce website for us. The design is modern, and the functionality is seamless. Our sales have significantly increased thanks to the smooth user experience they created. Their timely delivery and responsiveness made the entire process smooth and efficient. Highly recommend their services!",
+      name: "Anil Mehta",
+      title: "Atharv Mans Wear , Mumbai",
+    },
+    {
+      quote:
+"We are thrilled with the website Vedant's team developed for our salon. It’s sleek, fast, and easy to navigate. Our clients can now book appointments online effortlessly, which has improved our business operations. Their creative approach and technical skills are outstanding. We highly recommend their services!",
+      name: "Karan Patel",
+      title: "Style Hair Salon",
+    },
+    {
+      quote:
+"Vedant and his team did an excellent job redesigning our e-commerce platform. They ensured to understand our requirements and delivered a product that aligns perfectly with our brand. The new site is user-friendly and has enhanced our customer experience. Their expertise is evident in every aspect of the website, and we couldn't be more pleased.",
+      name: "Neeraj Joshi",
+      title: "EcoMarket,Nagpur",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "Wordpress",
+      img: "/wlogo.png",
+      nameImg: "/wp.png",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const WhatWeOffer = [
+    {
+      id: 1,
+      title: " Modern Web Development",
+      desc: "We build responsive, user-friendly websites that engage your audience and enhance your online presence.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Product Website Design",
+      desc: "Creative product websites designed with Next.js, coding expertise, GSAP animations, and Three.js graphics.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "E-commerce Website",
+      desc: "We offer ,Dynamic e-commerce websites built with Next.js, tailored for stores and various online businesses.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Graphic Design",
+      desc: "Elevate your brand with visually stunning designs, from logos to marketing materials, that captivate and communicate your message.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      src:"https://github.com/vedu8733"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      src:"https://x.com/Vedantpatil8733"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      src:"www.linkedin.com/in/vedantpatil8733"
+    },
+    {
+      id: 4,
+      img: "/insta.svg",
+      src:"https://www.instagram.com/vedantpatil8733"
+    },
+  ];
